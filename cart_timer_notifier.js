e7c243fe2cb8cefc4d453cf10e7328736a835c77
gazzamc <gazzaplays@gmail.com>
2023-10-05 13:09:38 +0200

    Added script for monitoring the cart

---
+++ cart_timer_notifier.js
@@ -0,0 +1,91 @@
+console.log("script loaded")
+
+const cartTimer = document.getElementsByClassName("expiration-timer")
+let timerStarted = false;
+let timeInterval;
+
+const debounce = (callback, wait) => {
+    let timeoutId = null;
+    return (...args) => {
+      window.clearTimeout(timeoutId);
+      timeoutId = window.setTimeout(() => {
+        callback.apply(null, args);
+      }, wait);
+    };
+  }
+
+  const sendToTelegram = debounce(() => {
+    console.log("Send To Telegram")
+    const message = `<a href="https://tickets.rugbyworldcup.com/en/cart">Tickets still in cart - ${getTime()} left to go!!</a>`;
+
+    const token = "<telegram-bot-token>";
+    // Get chat_id from https://api.telegram.org/bot<api-key>/getUpdates
+    const chat_id = "<telegram-chat-id>";
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}&parse_mode=html`;
+    
+    const oReq = new XMLHttpRequest();
+
+    try{
+        oReq.open("GET", url, true);
+        oReq.send();
+    } catch(err){
+        console.log(err)
+    }
+}, 10000);
+
+
+function checkTime(minutesLeft, secondsLeft) {
+    if(minutesLeft == 15){
+        sendToTelegram()
+        console.log(`Tickets added to cart, ${minutesLeft}:${secondsLeft} left. Hurry!!`)
+    } 
+
+    if(minutesLeft == 5){
+        sendToTelegram()
+        console.log("5 Mins left until Tickets Expire!!")
+    } 
+    
+    if(minutesLeft == 2){
+        sendToTelegram()
+        console.log("2 Mins left until Tickets Expire!!")
+    }
+
+    if(minutesLeft == 0 && secondsLeft <= 60){
+        sendToTelegram()
+        console.log("Less than a minute left until Tickets Expire!!")
+    }
+}
+
+function isTimer(time){
+    if(time > 0){
+        return true;
+    }
+
+    return false;
+}
+
+function getTime(){
+    return cartTimer[0].firstElementChild.innerText;
+}
+
+function startLoop(){
+    console.log("Looping")
+    if(cartTimer.length){
+        let time = cartTimer[0].firstElementChild.innerText
+        let minutesLeft = time.split(':')[0]
+        let secondsLeft = time.split(':')[1]
+    
+        checkTime(minutesLeft, secondsLeft)
+
+        if(timeInterval){
+            timeInterval = setInterval(startLoop, 10000);
+            sendToTelegram()
+        }
+
+    } else {
+        console.log("Times up or nothing in cart!!")
+        clearInterval(timeInterval);
+    }
+}
+
+startLoop();
