6e74284bc5f54ebeb452f1a072008df3ad2a4d80
gazzamc <gazzaplays@gmail.com>
2023-10-12 22:36:47 +0200

    Fixed issue that prevented redirect to card when selecting packs, changed button click to clickevent
    Prevented reloading while clicking buttons

--- add_tickets.js
+++ add_tickets.js
@@ -3,18 +3,18 @@ const DATE_TIME = new Date().toLocaleString();
 
 //Preferences
 const NO_TICKETS_WANTED = 2; // No of tickets wanted, will still add single tickets if available, this wont circumvent the hard limit (6) on the site.
-const MAX_PRICE = 350; //Maximum price per ticket (Not total), caution! any tickets lower than this price will not be added to basket, for best results don't make it too low
+const MAX_PRICE = 600; //Maximum price per ticket (Not total), caution! any tickets lower than this price will not be added to basket, for best results don't make it too low
 const PRIORITISE_MULTI = true; // Will sort by multi tickets and click those first
 const REDIRECT_TO_CART = true; // If you prefer the bot doesn't redirect after adding tickets to basket.
 
 //Some settings to try and reduce throttling
 const ENABLE_ANTI_THROTTLE = true; // Adds a delay between refreshes when we hit the throttle page eg (Loading...), helps with the slowdown error.
-const DELAY_CLICKS = 2000; // Delay between clicking cart buttons
-const DELAY_RELOAD = 20000; // Refresh delay on failure, no tickets
-const THROTTLE_REFRESH_DELAY = () => (Math.random() >= 0.5) ? 240000 : 120000; // Randomize the delay between 2 - 4 mins
-const THROTTLE_CHECK_DELAY = 0; // Delay before checking if we have been throttled // Needs to be long enough for page to load or we might miss the tickets // Adds delay to execution
+const DELAY_CLICKS = 1000; // Delay between clicking cart buttons
+const DELAY_RELOAD = (Math.floor(Math.random() * 101) * 2000); // Refresh delay on failure, no tickets
+const THROTTLE_REFRESH_DELAY = (Math.floor(Math.random() * 101) * 2000); // Randomize the delay between 2 - 4 mins
+const THROTTLE_CHECK_DELAY = 100; // Delay before checking if we have been throttled // Needs to be long enough for page to load or we might miss the tickets // Adds delay to execution
 const THROTTLE_TIMEOUT_TIME = 10000; // When checking for throttling we can timeout after a certain time, I think 10 secs is good as the server may be slow.
-const CHECK_CLICK_RESPONSE_TIMEOUT = 10; //Adds a delay when checking if click was successful, can add a delay to each click but will reduce false positives (someone grabs ticket first)
+const CHECK_CLICK_RESPONSE_TIMEOUT = 300; //Adds a delay when checking if click was successful, can add a delay to each click but will reduce false positives (someone grabs ticket first)
 
 //Misc
 const PERF = true; // Displays execution time between clicks
@@ -22,13 +22,20 @@ const DEBUG = true; // Adds debug messages, caution will increase bot execution 
 
 // Telegram settings
 const ENABLE_TELEGRAM = true;
-const TELEGRAM_TOKEN = "enter-your-token-here";
+const TELEGRAM_TOKEN = "replace-this-with-token";
 // Get chat_id from https://api.telegram.org/bot<api-key>/getUpdates
-const TELEGRAM_CHAT_ID = 123456; // Enter your chat id here, get it from the above url
+const TELEGRAM_CHAT_ID = 123456;
 
 // Don't modify below this line
 let start;
 let end;
+let redirecting = false;
+
+const clickEvent = new MouseEvent("click", {
+    "view": window,
+    "bubbles": true,
+    "cancelable": false
+});
 
 const debounce = (callback, wait) => {
     let timeoutId = null;
@@ -53,6 +60,8 @@ function getNoOfTicketsInCart() {
 }
 
 const redirectToCart = debounce(() => {
+    window.stop();
+
     if (DEBUG) {
         console.log("Redirect To Cart")
     }
@@ -168,15 +177,16 @@ function getAvailableTickets() {
     return 0;
 }
 
-function reloadPageWMsg(msg, noDate=false) {
-    console.log(`${msg}`, noDate ? null : DATE_TIME)
-    location.reload()
+function reloadPageWMsg(msg) {
+    console.log(`${msg}`, DATE_TIME)
+    window.location.reload(true)
 }
 
-function redirect(redirecting) {
+function redirect() {
+    redirecting = true
     const ticketsInCart = getNoOfTicketsInCart();
     // Double check we have tickets
-    if (ticketsInCart > 0 && !redirecting) {
+    if (ticketsInCart > 0) {
         if (DEBUG) {
             console.log("Tickets confirmed in cart!")
         }
@@ -188,7 +198,7 @@ function redirect(redirecting) {
                 redirectToCart()
             }
         }
-    } else if (!redirecting) { //Just it case it didn't detect the ticket, don't refresh
+    } else {
         if (DEBUG) {
             console.log("No tickets in cart, refreshing!")
         }
@@ -205,7 +215,6 @@ function startLoop() {
     const cartBtns_default = getAllCartButtons(); //Order of page
     let clickingDisabled = false;
     let multiExhausted = false;
-    let redirecting = false;
 
     let buttonTotal = cartBtns_default.length;
     let clickCount = 0;
@@ -232,14 +241,13 @@ function startLoop() {
         //If we meet our threshold then we redirect
         if (NO_TICKETS_WANTED == getNoOfTicketsInCart() && !redirecting) {
             clickingDisabled = true
-            redirecting = true
 
             if (DEBUG) {
                 console.log("No of tickets threshold matched, redirecting to cart!")
             }
 
             if (REDIRECT_TO_CART) {
-                redirect(redirecting);
+                redirect();
             }
         }
 
@@ -264,38 +272,35 @@ function startLoop() {
 
         if (detectErrorMessage("Max") && !redirecting) { // Max Allowed, redirect
             clickingDisabled = true
-            redirecting = true
 
             if (DEBUG) {
                 console.log("Max tickets, redirect to cart and/or send to telegram!")
             }
 
             if (REDIRECT_TO_CART) {
-                redirect(redirecting);
+                redirect();
             }
         }
 
         if (!redirecting && buttonTotal === clickCount && getNoOfTicketsInCart() > 0) { // Added at least one ticket to basket, all buttons clicked
-
             clickingDisabled = true
-            redirecting = true
 
             if (DEBUG) {
                 console.log("Clicked All buttons, redirecting to cart!")
             }
 
             if (REDIRECT_TO_CART) {
-                redirect(redirecting);
+                redirect();
             }
 
         };
     }
 
     function processItem(index) {
-        if (cartBtns_multi.length && !multiExhausted && NO_TICKETS_WANTED > 1 && !redirecting) {
+        if (cartBtns_multi.length && !multiExhausted && !redirecting) {
             // Try clicking until it fails, then reset
             try {
-                cartBtns_multi[index].click();
+                cartBtns_multi[index].dispatchEvent(clickEvent);
                 clickCount++
                 setTimeout(() => clickResponse(), CHECK_CLICK_RESPONSE_TIMEOUT);
             } catch (err) {
@@ -307,7 +312,7 @@ function startLoop() {
                 multiExhausted = true;
             }
         } else if (!redirecting) {
-            cartBtns_default[index].click()
+            cartBtns_default[index].dispatchEvent(clickEvent);
             clickCount++
 
             // wait a slight bit in order for error to appear
@@ -345,12 +350,25 @@ function startLoop() {
             }
         } else {
             if (DEBUG) {
-                console.log("Finished, No more buttons to click!")
+                console.log("Finished, No more buttons to click or Redirecting!")
             }
         }
     }
 
     if (buttonTotal === 0) { // No Tickets, redirect
+        //Check if cart exists
+        let cart = document.getElementById('edit-show-product-cart');
+        let ticketsInCart = getNoOfTicketsInCart();
+
+        if (cart && ticketsInCart > 0) {
+            if (DEBUG) {
+                console.log("%c Tickets in cart", "color: #ff0000");
+            }
+            if (REDIRECT_TO_CART) {
+                redirect();
+            }
+        }
+
         if (DEBUG) {
             console.log(`%c No Tickets within price range or none available, if the former, adjust max price setting`, 'color: #ff0000')
         }
@@ -384,8 +402,8 @@ setTimeout(() => {
 
 
         // Don't stop loading immediately to give time for page to resolve
-        setTimeout(() => { console.log(`Stop window`); window.stop() }, THROTTLE_TIMEOUT_TIME);
-        let waitTime = THROTTLE_REFRESH_DELAY();
+        setTimeout(() => { console.log(`Stop window`); window.stop() }, (THROTTLE_TIMEOUT_TIME > DELAY_RELOAD ? DELAY_RELOAD / 2 : THROTTLE_TIMEOUT_TIME));
+        let waitTime = THROTTLE_REFRESH_DELAY;
 
         if (document.getElementsByClassName('unavailable-page-visual').length) {
             if (DEBUG) {
